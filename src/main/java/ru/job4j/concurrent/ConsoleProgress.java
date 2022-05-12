243a4c9a3c2a7224c2f7e90455caf8006d802c0d
tigrdima <duys79yandex.ru>
2022-05-12 13:23:16 +0300

    3. Прерывание нити [#1019 #300108]

---
+++ src/main/java/ru/job4j/concurrent/ConsoleProgress.java
@@ -0,0 +1,29 @@
+package ru.job4j.concurrent;
+
+public class ConsoleProgress implements Runnable {
+
+    @Override
+    public void run() {
+        try {
+            String[] process = new String[]{"\\", "|", "/"};
+            int i = 0;
+            while (!Thread.currentThread().isInterrupted()) {
+                if (i > 2) {
+                    i = 0;
+                }
+                System.out.print("\r load: " + process[i++]);
+                Thread.sleep(500);
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread = new Thread(new ConsoleProgress());
+
+        thread.start();
+        Thread.sleep(1000);
+        thread.interrupt();
+    }
+}
