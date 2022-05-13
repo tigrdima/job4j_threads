9c3f0de05eb81f8ac3cf2c351a14b82b95599049
tigrdima <duys79yandex.ru>
2022-05-13 10:48:29 +0300

    4. Thread без общих ресурсов [#267919]

---
+++ src/main/java/ru/job4j/atomicity/UserCache.java
@@ -0,0 +1,54 @@
+package ru.job4j.atomicity;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+
+public class UserCache  {
+    private final ConcurrentHashMap<Integer, User> users = new ConcurrentHashMap<>();
+    private final AtomicInteger id = new AtomicInteger();
+
+    public void add(User user) {
+        users.put(id.incrementAndGet(), User.of(user.getName()));
+    }
+
+    public User findById(int id) {
+        return User.of(users.get(id).getName());
+    }
+
+    public List<User> findAll() {
+        return users.values()
+                .stream()
+                .map(user -> User.of(user.getName()))
+                .collect(Collectors.toList());
+    }
+}
+
+class User {
+    private int id;
+    private String name;
+
+    public static User of(String name) {
+        User user = new User();
+        user.name = name;
+        return user;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
