d8c5353565763aad7c6883d5672447e04d5ead68
tigrdima <duys79yandex.ru>
2022-05-13 10:09:41 +0300

    4. Скачивание файла с ограничением. [#144271 #300159]

--- src/main/java/ru/job4j/concurrent/Wget.java
+++ src/main/java/ru/job4j/concurrent/Wget.java
@@ -8,41 +8,69 @@ import java.util.Calendar;
 
 public class Wget implements Runnable {
     private final String url;
+    private final String toPath;
     private final int speed;
+    private static long startLoading;
 
-    public Wget(String url, int speed) {
+    public Wget(String url, String toPath, int speed) {
         this.url = url;
+        this.toPath = toPath;
         this.speed = speed;
     }
 
     @Override
     public void run() {
         try (BufferedInputStream in = new BufferedInputStream(new URL(url).openStream());
-             FileOutputStream fileOutputStream = new FileOutputStream("pom_tmp.xml")) {
+             FileOutputStream fileOutputStream = new FileOutputStream(toPath)) {
             byte[] dataBuffer = new byte[1024];
             int bytesRead;
+            long bytesWrite = 0;
+            long start = Calendar.getInstance().getTimeInMillis();
+            startLoading = start;
 
             while ((bytesRead = in.read(dataBuffer, 0, 1024)) != -1) {
-                long start = Calendar.getInstance().getTimeInMillis();
+                bytesWrite += bytesRead;
                 fileOutputStream.write(dataBuffer, 0, bytesRead);
-                long end = Calendar.getInstance().getTimeInMillis();
-                System.out.print("\rloading ...");
-                if ((end - start) < speed) {
-                    Thread.sleep(speed);
+
+                if (bytesWrite >= speed) {
+                    long end = Calendar.getInstance().getTimeInMillis();
+                    long deltaTime = end - start;
+                    start = end;
+                    bytesWrite = 0;
+                    if (deltaTime < 1000) {
+                        Thread.sleep(1000 - deltaTime);
+                    }
                 }
             }
-        } catch (IOException | InterruptedException e) {
+        } catch (IOException e) {
             e.printStackTrace();
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    private static void validate(String[] args) {
+        if (args.length != 3) {
+            throw new IllegalArgumentException("""
+                    The number of arguments must be three.\r
+                    sample :\r
+                    first argument(URL) - "https://adress.net/files/111.txt"\r
+                    second argument(path to download) - c:/dir/file.txt\r
+                    third argument(MByte/s) - 1048576""");
         }
     }
 
     public static void main(String[] args) throws InterruptedException {
-        String url = args[0];
-        int speed = Integer.parseInt(args[1]);
+        validate(args);
 
-        Thread wget = new Thread(new Wget(url, speed));
+        String url = args[0];
+        String toPath = args[1];
+        int speed = Integer.parseInt(args[2]);
+
+        Thread wget = new Thread(new Wget(url, toPath, speed));
         wget.start();
         wget.join();
         System.out.println("\r\nfile downloaded");
+        System.out.println("loading time : " + ((Calendar.getInstance().getTimeInMillis() - startLoading)) / 1000 + " sec");
     }
 }
