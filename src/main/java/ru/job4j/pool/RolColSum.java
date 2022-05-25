0c7ad6bdbadc29cfe0ede31615751add76eac0ed
tigrdima <duys79yandex.ru>
2022-05-25 08:14:37 +0300

    4. CompletableFuture [#361626 #304547]

---
+++ src/main/java/ru/job4j/pool/RolColSum.java
@@ -0,0 +1,38 @@
+package ru.job4j.pool;
+
+public class RolColSum {
+    public static class Sums {
+        private int rowSum;
+        private int colSum;
+
+        public int getRowSum() {
+            return rowSum;
+        }
+
+        public void setRowSum(int rowSum) {
+            this.rowSum = rowSum;
+        }
+
+        public int getColSum() {
+            return colSum;
+        }
+
+        public void setColSum(int colSum) {
+            this.colSum = colSum;
+        }
+    }
+
+//    public static Sums[] sum(int[][] matrix) {
+//
+//        for (int i = 0; i < matrix.length; i++) {
+//            for (int j = 0; j < matrix[i].length; j++) {
+//
+//            }
+//        }
+//
+//    }
+//
+//    public static Sums[] asyncSum(int[][] matrix) {
+//
+//    }
+}
